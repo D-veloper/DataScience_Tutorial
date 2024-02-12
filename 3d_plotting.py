f3467f5dd09fc6f431570b432e8b9adafcc4ed6c
pcnnadi <psxpn2@nottingham.ac.uk>
2024-02-12 15:54:21 +0000

    plotting surface plots

--- 3d_plotting.py
+++ 3d_plotting.py
@@ -4,10 +4,19 @@ from mpl_toolkits import mplot3d
 
 axis = plt.axes(projection="3d")
 
-z = np.linspace(0, 30, 100)
-x = np.sin(z)
-y = np.cos(z)
+# z = np.linspace(0, 30, 100)
+# x = np.sin(z)
+# y = np.cos(z)
 
-axis.plot3D(x, y, z)
+def z_function(x, y):
+    return np.sin(np.sqrt(x ** 2 + y ** 2))
+
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+
+X, Y = np.meshgrid(x, y)
+Z = z_function(X, Y)
+
+axis.plot_surface(X, Y, Z)
 
 plt.show()
